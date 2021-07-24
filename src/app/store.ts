3cf7c4d6a90ee561cb23e9a6f997c62072ea49ff
Aser <techdr95@gmail.com>
2021-07-23 23:13:41 -0400

    Create a rough draft component that gets characters based off user input. This component also is responsive

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,14 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { characterApi } from '../features/character/characterSlice';
 import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [characterApi.reducerPath]: characterApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(characterApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
---
+++ src/features/character/Character.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import ListGroup from 'react-bootstrap/ListGroup'
+
+
+interface Props {
+    name: string;
+    image: string;
+    // status: string;
+    species: string;
+    gender: string;
+}
+
+export const Character = (props: Props) => {
+    return (
+        <div >
+            <Card style={{ width: '10rem' }}>
+  <Card.Img variant="top" src={props.image} />
+  <Card.Body>
+    <Card.Title>{props.name}</Card.Title>
+    <Card.Text>
+    Name: {props.name}
+    </Card.Text>
+    <Card.Text>
+    Species: {props.species}
+    </Card.Text>
+    <Card.Text>
+    Gender: {props.gender}
+    </Card.Text>
+    {/* <ListGroup>
+                        <ListGroup.Item>Name: {props.name}</ListGroup.Item>
+                        <ListGroup.Item>Species: {props.species}</ListGroup.Item>
+                        <ListGroup.Item>Gender: {props.gender}</ListGroup.Item>
+</ListGroup> */}
+  </Card.Body>
+</Card>
+        </div>
+    )
+}
