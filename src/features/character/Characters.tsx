825a057ce38f9b68ac0aae8ae41161fd1a88952e
Aser <techdr95@gmail.com>
2021-07-25 21:37:04 -0400

    Some tweeks with the card ui.

--- src/features/character/Characters.tsx
+++ src/features/character/Characters.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { Character } from './Character'
 import { useGetCharacterByIdQuery, useGetCharacterByNameQuery } from './characterSlice'
 import './Characters.css';
+import Accordion from 'react-bootstrap/Accordion'
 
 export default function Characters () {
     // const { data } = useGetCharacterByIdQuery(1)
@@ -18,18 +19,19 @@ export default function Characters () {
     return (
         <div style={{padding: '2em'}}>
             <form onSubmit={getCharacter}>
-                <input type='text' name='findChar' placeholder='Enter Character Name'/>
+                <input style={{color: 'white', background: '#212529', width: '12rem'}} type='text' name='findChar' placeholder='Enter Character Name'/>
             </form>
             {console.log(data)}
             <div className='wrapper'>
            {isLoading && 'Loading...'}
            {isError && error}
-                {isSuccess && data && data?.results.map(user => <
-                    Character name={user.name}
+                    {isSuccess && data && data?.results.map(user =>
+                        <Character name={user.name}
                     image={user.image}
                     species={user.species}
                     gender={user.gender}
-                />)}
+                        />
+                    )}
             </div>
             
       </div>
